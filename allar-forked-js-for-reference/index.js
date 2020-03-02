a65405bdec2b80deefb5badb9dedf089cd4dc6c5
Mike Seese <seesemichaelj@gmail.com>
2020-03-01 17:24:56 -0800

    remove reference code

--- allar-forked-js-for-reference/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-const argv = require("yargs").argv;
-
-if (argv.manifests) {
-  require("./manifests.js");
-} else {
-  require("./downloader.js");
-}
--- allar-forked-js-for-reference/ue4-mp-downloader.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-var mpdownloader = require("./index.js");
